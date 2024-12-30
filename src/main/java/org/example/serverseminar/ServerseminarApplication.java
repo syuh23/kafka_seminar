f5bbe61c15b16af23c69795df37ebf527c5ea1c6
syuh23 <seonghee5070@gmail.com>
2024-12-30 18:16:57 +0900

    kafka start setting

---
+++ src/main/java/org/example/serverseminar/ServerseminarApplication.java
@@ -0,0 +1,16 @@
+package org.example.serverseminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerseminarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerseminarApplication.class, args);
+
+        // 여기서 코드 쳐보면서 한번 시작점을 찾아보려고 했음
+
+    }
+
+}
