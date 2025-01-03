44b5b94247ca2321d54b8b8fecd94226bfbd4912
syuh23 <seonghee5070@gmail.com>
2025-01-03 17:01:59 +0900

    kafka connection error edit dependencies

--- src/main/java/org/example/serverseminar/ServerseminarApplication.java
+++ src/main/java/org/example/serverseminar/ServerseminarApplication.java
@@ -1,5 +1,6 @@
 package org.example.serverseminar;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +9,7 @@ public class ServerseminarApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerseminarApplication.class, args);
-
+        KafkaProducer kafkaProducer;
         // 여기서 코드 쳐보면서 한번 시작점을 찾아보려고 했음
 
     }
