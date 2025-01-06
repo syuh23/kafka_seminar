6559c9d9527c0ea2d93e59b487e2e2b4796ed222
SEUNGHEE\seong <seonghee5070@gmail.com>
2025-01-07 02:26:45 +0900

    search kafka starting point 1

--- src/main/java/org/example/serverseminar/ServerseminarApplication.java
+++ src/main/java/org/example/serverseminar/ServerseminarApplication.java
@@ -3,15 +3,15 @@ package org.example.serverseminar;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class ServerseminarApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerseminarApplication.class, args);
-        KafkaProducer kafkaProducer;
         // 여기서 코드 쳐보면서 한번 시작점을 찾아보려고 했음
-
+        KafkaProducer<?, ?> kafkaProducer;
+        KafkaTemplate<?, ?> kafkaTemplate;
     }
-
 }
