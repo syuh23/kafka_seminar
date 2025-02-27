f6c9448da3e1080c5520686fd451f37b39e3bcd0
syuh_mbp <seonghee5070@gmail.com>
2025-02-27 17:00:30 +0900

    add request topic in producer conrtoller

--- src/main/java/com/example/springkafkapt/controller/ConsumerService.java
+++ src/main/java/com/example/springkafkapt/controller/ConsumerService.java
@@ -24,38 +24,42 @@ public class ConsumerService {
     // 각 동적 컨슈머(KafkaMessageListenerContainer 인스턴스)를 관리하기 위한 Map
     private final Map<String, KafkaMessageListenerContainer<String, String>> consumerContainers = new ConcurrentHashMap<>();
 
-    private final List<String> KafkaMessageList = new ArrayList<>();
+    private final List<String> kafkaMessageList = new ArrayList<>();
 
 
     public ConsumerService(ConsumerConfigCustom consumerConfigCustom) {
         this.consumerConfigCustom = consumerConfigCustom;
     }
 
-    public void createConsumer(String topic, String groupId) {  // kafka consumer 생성
 
+    // kafka consumer 생성
+    public void createConsumer(String topic, String groupId) {
+        List<String> messageList = new ArrayList<>();
         String clientId = UUID.randomUUID().toString();  // 고유한 cliendId 생성, Kafka 브로커와의 통신 및 파티션 할당 시 각 컨슈머를 고유하게 식별하는 데 사용
-        String key = groupId + "_" + topic + "_" + clientId;  // cliendId와 groupId, topic을 조합하여 고유 key값 생성
+        String key = groupId + "_" + topic + "_" + clientId;  // 고유 key값 생성
 
-        ConsumerFactory<String, String> consumerFactory = consumerConfigCustom.createConsumerFactory(groupId, clientId);  // consumer 생성
+        // consumer 생성
+        ConsumerFactory<String, String> consumerFactory = consumerConfigCustom.createConsumerFactory(groupId, clientId);
 
-        ContainerProperties containerProps = new ContainerProperties(topic);  // 구독할 topic 설정
+        ContainerProperties containerProps = new ContainerProperties(topic);
+
         containerProps.setMessageListener(new MessageListener<String, String>() {
             @Override
             public void onMessage(ConsumerRecord<String, String> record) {
                 String message = "들어온 메시지 확인 >>>  토픽 : " + record.topic() + ", 메시지 : " + record.value();
                 System.out.println(message);
-                KafkaMessageList.add(record.value());
+                messageList.add(record.value());
             }
         });
 
         KafkaMessageListenerContainer<String, String> container = new KafkaMessageListenerContainer<>(consumerFactory, containerProps);
         container.start();
         consumerContainers.put(key, container);
+        kafkaMessageList.addAll(messageList);
     }
 
-
     public List<String> getAllMessages() {
-        return new ArrayList<>(KafkaMessageList);
+        return new ArrayList<>(kafkaMessageList);
     }
 
 }
